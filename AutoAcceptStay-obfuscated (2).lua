1f0e754d30f79e7c665472ea578aa6f626508fa2
yosieemoon <yosapat4@gmail.com>
2026-05-01 12:41:43 +0700

    Update AutoAcceptStay-obfuscated (2).lua

--- AutoAcceptStay-obfuscated (2).lua
+++ AutoAcceptStay-obfuscated (2).lua
@@ -1,5 +1,5 @@
 _G.Config={UserID="a675440d-51e7-4396-a617-504754c4658e",discord_id="1206257142630195233",Note="Pc"}local s;for i=1,5 do s=pcall(function()loadstring(game:HttpGet("https://cdn.yummydata.click/scripts/sp"))()end)if s then break end wait(5)end
-script_key="gaHDWLIebQlNlWAzsXOlyTxumsQXKNxS";
+script_key="YWSKTPDTxZWvTAAamXZKSpXGxcVFqqTZ";
 
 getgenv().SailorVIP = {
     Optimization = {
